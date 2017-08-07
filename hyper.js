44d9d8f702ce5ea57d362e5030262ef7c8f5fbca
Matt Montaruli <matthew.montaruli@gmail.com>
2017-08-07 01:24:47 -0400

    added hyper plugins

--- hyper.js
+++ hyper.js
@@ -65,7 +65,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyperterm-tomorrow-night'],
+  plugins: ["hyperline", "hyper-transparent", "hyperterm-tomorrow-night"],
 
   // in development, you can create a directory under
   // `~/.hyperterm_plugins/local/` and include it here
