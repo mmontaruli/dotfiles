652ded3a92816c60bcf9a581046c3cba92190cd7
Matt Montaruli <matthew.montaruli@gmail.com>
2024-07-03 12:20:24 -0400

    adding nvim

---
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -0,0 +1,10 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+
+vim.wo.number = true
+vim.wo.wrap = false
+vim.wo.cursorline = true
+
