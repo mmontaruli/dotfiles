62cc881a7b36e294fc64d4018b779e3571012f6f
mmontaruli <matthew.montaruli@gmail.com>
2024-07-03 16:28:33 -0400

    fixed zellij and neotree can now see dotfiles

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,4 +1,4 @@
-return  {
+return {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v3.x",
   dependencies = {
@@ -8,5 +8,16 @@ return  {
   },
   config = function()
     vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal left<CR>', {})
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,        -- This makes hidden files visible
+          hide_dotfiles = false, -- This shows dotfiles (files starting with a dot)
+          never_show = {
+            ".git",
+          },
+        },
+      },
+    })
   end
 }
