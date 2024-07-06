b3e24b7c1c442a71403e1e50c499b6c658805d6a
mmontaruli <matthew.montaruli@gmail.com>
2024-07-06 16:29:35 -0400

    changed nvim config for telescope to include hidden files

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -4,7 +4,10 @@ return  {
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       local builtin = require("telescope.builtin")
-      vim.keymap.set('n', '<C-i>', builtin.find_files, {})
+      -- vim.keymap.set('n', '<C-i>', builtin.find_files, {})
+      vim.keymap.set('n', '<C-i>', function()
+        builtin.find_files({ hidden = true })
+      end, {})
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     end
   },
