e828056b9326878fd68cec6eeba0a3bdb0ad92ea
Matt Montaruli <matthew.montaruli@gmail.com>
2024-07-11 11:04:04 -0400

    added a few new nvim options

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -2,9 +2,17 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.opt.smartindent = true
+vim.opt.breakindent = true
 vim.g.mapleader = " "
 
 vim.wo.number = true
 vim.wo.wrap = false
 vim.wo.cursorline = true
 vim.o.fillchars = 'eob: '
+
+--vim.opt.nu = true
+--vim.opt.rnu = true
+
+vim.opt.signcolumn = "yes"
+vim.opt.scrolloff = 8
