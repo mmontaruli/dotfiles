b4482f304c8d825ee6d245cd90c59c17e2c58bf3
Matt Montaruli <matthew.montaruli@gmail.com>
2024-07-16 15:04:17 -0400

    fixed solargraph setup

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,47 +1,52 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
     opts = {
       auto_install = true,
     },
-		-- config = function()
-		-- 	require("mason-lspconfig").setup({
-		-- 		ensure_installed = { "lua_ls", "tsserver", "solargraph", "html", "tailwindcss" },
-		-- 	})
-		-- end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities
-			})
-			lspconfig.tsserver.setup({
-				capabilities = capabilities
-			})
-			lspconfig.solargraph.setup({
-				capabilities = capabilities
-			})
-			lspconfig.html.setup({
-				capabilities = capabilities
-			})
-			lspconfig.tailwindcss.setup({
-				capabilities = capabilities
-			})
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+    -- config = function()
+    -- 	require("mason-lspconfig").setup({
+    -- 		ensure_installed = { "lua_ls", "tsserver", "solargraph", "html", "tailwindcss" },
+    -- 	})
+    -- end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.tsserver.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.solargraph.setup({
+        capabilities = capabilities,
+        settings = {
+          solargraph = {
+            diagnostics = false,
+          },
+        },
+      })
+      lspconfig.html.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.tailwindcss.setup({
+        capabilities = capabilities,
+      })
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
