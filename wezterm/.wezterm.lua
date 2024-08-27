e1634d763b68ad5e749192f3dea19946722b1f34
mmontaruli <matthew.montaruli@gmail.com>
2024-08-27 14:50:50 -0400

    updated wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,10 +7,12 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
+-- config.color_scheme = 'Tokyo Night'
 config.color_scheme = 'Catppuccin Mocha'
 config.hide_tab_bar_if_only_one_tab = true
-
-wezterm.font('JetBrainsMono Nerd Font Mono Regular', {weight = 'Medium', stretch = 'Normal', style = 'Normal'})
-
+config.font_size = 18
+config.initial_cols = 130
+config.initial_rows = 35
+config.window_decorations = "RESIZE"
 -- and finally, return the configuration to wezterm
 return config
