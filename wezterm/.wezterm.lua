654195e01bf6bee15b1dee186acbb7c12db7d0a8
mmontaruli <matthew.montaruli@gmail.com>
2024-09-26 11:55:35 -0400

    removed transparency from wezterm and window borders

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -15,17 +15,17 @@ config.initial_cols = 130
 config.initial_rows = 35
 config.window_decorations = "RESIZE"
 config.window_decorations = "MACOS_FORCE_ENABLE_SHADOW"
-config.window_background_opacity = 0.85
-config.macos_window_background_blur = 65
-config.window_frame = {
-  border_left_width = '6px',
-  border_right_width = '6px',
-  border_bottom_height = '6px',
-  border_top_height = '6px',
-  border_left_color = "#F38BA8",
-  border_right_color = "#F38BA8",
-  border_top_color = "#F38BA8",
-  border_bottom_color = "#F38BA8",
-}
+--config.window_background_opacity = 0.85
+-- config.macos_window_background_blur = 65
+-- config.window_frame = {
+--  border_left_width = '6px',
+--   border_right_width = '6px',
+--   border_bottom_height = '6px',
+--   border_top_height = '6px',
+--   border_left_color = "#F38BA8",
+--   border_right_color = "#F38BA8",
+--   border_top_color = "#F38BA8",
+--   border_bottom_color = "#F38BA8",
+-- }
 -- and finally, return the configuration to wezterm
 return config
