d1c60cb0cc640e96a3f111cbde944d754e1b79c9
mmontaruli <matthew.montaruli@gmail.com>
2024-10-07 16:05:33 -0400

    fixed wezterm resize issue and added telescope find buffers in nvim

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -11,6 +11,7 @@ return  {
         })
       end, {})
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<CR>', { desc = 'Find Buffers' })
     end
   },
   {
