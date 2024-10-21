e28b87173e503b654fcebfff67da272a68ce2a03
mmontaruli <matthew.montaruli@gmail.com>
2024-10-21 12:43:28 -0400

    made a few updates to wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -14,18 +14,16 @@ config.font_size = 18
 config.initial_cols = 130
 config.initial_rows = 35
 config.window_decorations = "RESIZE | MACOS_FORCE_ENABLE_SHADOW"
--- config.window_decorations = "MACOS_FORCE_ENABLE_SHADOW"
---config.window_background_opacity = 0.85
--- config.macos_window_background_blur = 65
--- config.window_frame = {
---  border_left_width = '6px',
---   border_right_width = '6px',
---   border_bottom_height = '6px',
---   border_top_height = '6px',
---   border_left_color = "#F38BA8",
---   border_right_color = "#F38BA8",
---   border_top_color = "#F38BA8",
---   border_bottom_color = "#F38BA8",
--- }
--- and finally, return the configuration to wezterm
+config.keys = {
+  {
+    key = 'f',
+    mods = 'CTRL',
+    action = wezterm.action.ToggleFullScreen,
+  },
+  -- {
+  --   key = '/',
+  --   mods = 'CTRL',
+  --   action = wezterm.action.ClearScrollback 'ScrollbackAndViewport',
+  -- },
+}
 return config
